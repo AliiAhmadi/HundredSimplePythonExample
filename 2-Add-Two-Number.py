283556900087071d6b2b0cf37e8160f30535fc85
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-22 22:35:57 +0330

    Add-Two-Number

---
+++ 2-Add-Two-Number.py
@@ -0,0 +1,8 @@
+# In this program we should get two number from command line and add them to each other and print result
+
+firstNumber = float(input())
+secondNumber = float(input())
+
+sumOfNumbers = firstNumber + secondNumber
+
+print(sumOfNumbers)
