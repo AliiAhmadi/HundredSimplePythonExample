c634b95ad0618ba52ba118b2753858506956a98a
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-22 23:02:56 +0330

    QuadraticEquation

---
+++ 5-QuadraticEquation.py
@@ -0,0 +1,12 @@
+# a(x**2) + bx + c = 0
+
+a = 1
+b = 5
+c = 6
+
+delta = (b ** 2) - (4 * a * c)
+
+first = (-b + (delta ** .5)) / 2 * a
+second = (-b - (delta ** .5)) / 2 * a
+
+print(first, second)
